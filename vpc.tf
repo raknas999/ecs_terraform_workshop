64a17f196f7e361dac7f1eeaaf091c86d6453657
raknas999 <32408560+raknas999@users.noreply.github.com>
2022-03-22 20:38:43 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -3,11 +3,11 @@ module "vpc" {
   version        = "3.13.0"
   name           = "test_ecs_provisioning"
   cidr           = "10.0.0.0/16"
-  azs            = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  azs            = ["us-east-2a", "us-east-2b", "us-east-2c"]
   public_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   tags = {
     "env"       = "dev"
-    "createdBy" = "mkerimova"
+    "createdBy" = "sankar"
   }
 
 }
